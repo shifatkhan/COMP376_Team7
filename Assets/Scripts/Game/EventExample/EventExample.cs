d422553c340b11c053e8a7ab128984bc9ea5b24d
shifatkhan <shifatkhan6@gmail.com>
2020-10-30 13:20:13 -0400

    Added Game Event + example

---
+++ Assets/Scripts/Game/EventExample/EventExample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventExample : MonoBehaviour
+{
+    public GameEvent buttonClickedEvent;
+
+    public void ButtonClicked()
+    {
+        buttonClickedEvent.Raise();
+    }
+}
