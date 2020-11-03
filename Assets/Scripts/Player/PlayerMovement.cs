030aed526ad2b3bc927f8c38e764be48cf8a91a2
shifatkhan <shifatkhan6@gmail.com>
2020-11-03 18:36:16 -0500

    Added rigidBody + made a player prefab

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,30 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(PlayerInput))]
-[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player stats")]// TODO: Replace this with a ScriptableObject.
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 3.5f;
-    [SerializeField] private float gravity = 9.81f;
     [SerializeField] private float turnSpeed = 50f;
-    private float movementY; // Calculates Y velocity and movement.
 
-    private CharacterController characterController;
     private PlayerInput playerInput;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
     }
 
     private void Update()
     {
         UpdateFaceDirection();
-        Jump();
     }
 
     private void FixedUpdate()
@@ -33,28 +28,21 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    /// <summary>
+    /// Move the player according to the world space.
+    /// </summary>
     private void Move()
     {
         // Add move speed.
         Vector3 movement = playerInput.directionalInput * moveSpeed;
 
-        // Calculate gravity.
-        movementY -= gravity * Time.deltaTime;
-        movement.y = movementY;
-
         // Move.
-        characterController.Move(movement * Time.deltaTime);
+        transform.Translate(movement * Time.deltaTime, Space.World);
     }
 
-    // TODO: remove jump
-    private void Jump()
-    {
-        if (playerInput.jumpInput && characterController.isGrounded)
-        {
-            movementY = jumpForce;
-        }
-    }
-
+    /// <summary>
+    /// Rotate the player towards the direction it is moving.
+    /// </summary>
     private void UpdateFaceDirection()
     {
         if (playerInput.directionalInput.magnitude >= 0.1f)
