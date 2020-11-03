94113a220e701051dabd314aa974b803f2a0e6b0
Taqi Haque <taqi_haque@hotmail.com>
2020-11-03 18:40:28 -0500

    Update

---
+++ Assets/Scripts/Game/FloorHazards.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorHazards : MonoBehaviour
+{
+    public GameObject waterPuddle; //Prefab water
+
+    void Start() 
+    {
+        //Spawn Puddle
+        InvokeRepeating("SpawnPuddle", Random.Range(10, 15), Random.Range(10, 20)); //1st puddle will appear between 10 to 15 sec, the following puddles will appear every 10 to 20 seconds
+        
+    }
+    void SpawnPuddle()
+    {
+        //Spawn puddle at a random x, z location
+        float xRandom = Random.Range(-8, 8);
+        float y = 0.25f;
+        float zRandom = Random.Range(-4.7f, 1);
+        
+        Vector3 spawnPosition = new Vector3(xRandom, y, zRandom);
+        GameObject newPuddle = Instantiate(waterPuddle, spawnPosition, Quaternion.identity) as GameObject;
+        newPuddle.transform.Rotate(0, Random.Range(0, 360), 0, Space.World); //Random y rotation
+    }
+}
---
+++ Assets/Scripts/Game/TriggerZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Trigger Enter");
+    }
+    
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("Trigger Stay");
+    }
+    
+    void OnTriggerExit(Collider col)
+    {
+        Debug.Log("Trigger Exit");
+    }
+}
