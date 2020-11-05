8a5db13a33c3ac76cb7ba7bb03f48aa523161877
Newt vo <nhutmvo1997@gmail.com>
2020-11-04 21:37:58 -0500

    Add Table and Table Manager Script

---
+++ Assets/RawModels/Low Poly Random Food And Objects Pack/readme.txt
@@ -0,0 +1,15 @@
+#Lowpoly Pixel Objects
+
+This pack contains 71 models from scenes made for each day of december, following prompts from Low Poly Dailies.  Perhaps it's better to call them an artbook of sorts rather than an asset pack.
+
+Polygon count is below 500 tris per model, texture size varies from 16x16px to 64x32px. Included modesl are listed in contents.txt.
+
+For best results make sure the texture compression in your engine is set to lossless. Distortions become very noticeable on tiny textures, similar to pixelart sprites.
+
+For upcoming assets and game projects to come, follow my twitter.
+
+Distributed under CC-BY-4.0 License. 
+Please link back to: 
+    https://librorumque.itch.io/3december-2019
+
+Enjoy!
---
+++ Assets/Script/TableManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//- Have a TableManager script: This will randomly assign clients to a table (we dont need to show the client models
+//for now. We can just make the table change color to show that it is occupied)
+
+//Hmm so what I think we should do is have an Array of GameObjects[] called tables
+//We will populate this array in the inspector by order of the table numbers (each table will have a unique number).
+//Then, inside the TableManager script, you can do something like (in pseudo code)
+//Update(){
+//    every Random.range(0, maxInterval) seconds{
+//        int i = Random.range(0, tables.size);
+//        tables[i].EnableCustomers(): 
+//  }
+//}
+
+//Where EnableCustomers can be a function that changes the color of that table
+public class TableManager : MonoBehaviour
+{
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public GameObject[] tables;
+
+    private bool seated = false;                    //If the table already has a group on it
+    //private float timer = 30;                        //Timers when customers are seated
+
+    public float maxTime = 15;
+    public float minTime = 2; 
+
+    private float time;                             //current time
+    private float spawnTime;
+
+  
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRandomTime();
+        time = minTime;
+    }
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Update is called once per frame
+    void Update()
+    {
+        //Counts up
+        time += Time.deltaTime;
+ 
+        // Check to randomly choose table to be occupied
+        if (time >= spawnTime)
+        {
+            int i = Random.Range(0, tables.Length);
+            Debug.Log(tables[i]);
+            tables[i].GetComponent<Table>().EnableCustomers();
+            //EnableCustomers();
+            time = minTime;
+        }
+        SetRandomTime();
+
+    }
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public void SetRandomTime()
+    {
+        spawnTime = Random.Range(minTime, maxTime);
+    }
+
+}
