243d9a16439669f60821638ee520a55aa72bf557
Akisukei <justin321261@hotmail.com>
2020-11-13 14:51:40 -0500

    updated objects to have better colliders

--- Assets/Scripts/Water/WaterCheckBar.cs
+++ Assets/Scripts/Water/WaterCheckBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PourWater : MonoBehaviour
+public class WaterCheckBar : MonoBehaviour
 {
     [SerializeField]
     float fillSpeed = 0.5f;
--- Assets/Scripts/Water/WaterPourable.cs
+++ Assets/Scripts/Water/WaterPourable.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TableInteractTemp : MonoBehaviour
+public class WaterPourable : MonoBehaviour
 {
     [SerializeField]
-    Transform patienceStatusPrefab;
+    Transform waterStatusPrefab;
     [SerializeField]
-    Transform skillCheckPrefab;
+    Transform waterCheckPrefab;
+    [SerializeField]
+    float drainRate = 0.1f;
 
     private GameObject canvasRef;
     private Transform skillCheckObj;
     private Transform waterCupBarObj;
+    private Slider slider;
+
     private bool skillChecking = false;
 
     void Start()
@@ -19,33 +24,48 @@ public class TableInteractTemp : MonoBehaviour
         canvasRef = GameObject.Find("Canvas - Game");
 
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        waterCupBarObj = Instantiate(patienceStatusPrefab, screenPos, Quaternion.identity);
+        waterCupBarObj = Instantiate(waterStatusPrefab, screenPos, Quaternion.identity);
         waterCupBarObj.SetParent(canvasRef.transform);
+
+        slider = waterCupBarObj.GetComponent<Slider>();
+        slider.value = 1f;
     }
 
     void Update()
     {
+        // make water cup bar follow table when camera follows player
+
         if (skillChecking)
         {
             waterCupBarObj.gameObject.SetActive(false);
 
+            // enable water draining again if done skill check
             if (skillCheckObj == null)
                 skillChecking = false;
         }
         else if (!skillChecking)
         {
             waterCupBarObj.gameObject.SetActive(true);
+
+            if (slider.value > 0)
+                slider.value -= drainRate * Time.deltaTime;
         }
     }
 
-    void OnTriggerStay(Collider other)
+    public void waterFilled()
     {
+        slider.value = 1f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.LogWarning("HERE");
         if (Input.GetButton("Interact") && other.CompareTag("Player") && !skillChecking)
         {
             skillChecking = true;
 
             Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-            skillCheckObj = Instantiate(skillCheckPrefab, screenPos, Quaternion.identity);
+            skillCheckObj = Instantiate(waterCheckPrefab, screenPos, Quaternion.identity);
             skillCheckObj.SetParent(canvasRef.transform);
         }
     }
