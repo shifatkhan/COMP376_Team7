b82d71292fcfea151913a920d6b290beba805b2d
Newt vo <nhutmvo1997@gmail.com>
2020-11-26 14:37:49 -0500

    Create GameManager Script

---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager _instance;
+
+
+    // Singleton pattern implementation
+    //public static GameManager Instance
+    //{
+    //    get
+    //    {
+    //        if (_instance == null)
+    //        {
+    //            _instance = FindObjectOfType<GameManager>();
+    //            if (_instance == null)
+    //            {
+    //                GameObject go = new GameObject();
+    //                go.name = typeof(GameManager).Name;
+    //                _instance = go.AddComponent<GameManager>();
+    //                DontDestroyOnLoad(go);
+    //            }
+    //        }
+    //        return _instance;
+    //    }
+    //}
+
+    //private void Awake()
+    //{
+    //    if (_instance == null)
+    //    {
+    //        _instance = this;
+    //        DontDestroyOnLoad(this.gameObject);
+    //    }
+    //    else
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TableManagerJames tableManager = TableManagerJames.Instance;
+        CustomerManagerJames customerManager = CustomerManagerJames.Instance;
+        PuddleManagerJames puddleManager = PuddleManagerJames.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
