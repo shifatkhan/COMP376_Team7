678e77a218792f0c8664bdedea69ea61383407e6
Newt vo <nhutmvo1997@gmail.com>
2020-12-01 19:05:35 -0500

    Add HoldingItem script to check for player's collider and puddle

---
+++ Assets/Scripts/Player/HoldingItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingItem : MonoBehaviour
+{
+    public GameObject Mop;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (Mop.transform.parent != null && Mop.transform.parent.name == "PickupObject")
+        {
+            Debug.Log("Mop's Parent: " + Mop.transform.parent.name);
+            if (other.CompareTag("Puddle"))
+            {
+                // TODO: Usually we check if the mop is in the player's hand, but what if the player throws the mop on a puddle?
+                //      Also, instead of waiting 2 seconds before destroying, instantiate a particle onDestroy.
+               
+                Destroy(other.gameObject);
+            }
+        }
+       
+    }
+
+    // TODO: Check item to see which current item the player is holding
+    void checkItem()
+    {
+       
+    }
+}
