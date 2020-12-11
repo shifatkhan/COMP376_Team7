6e2684833185c60b48ee1b075be3d831ecf6c40f
Newt vo <nhutmvo1997@gmail.com>
2020-12-10 22:46:39 -0500

    Add Offscreen Indicators

--- Assets/Scripts/Offscreen Indicator/OffScreenIndicator.cs
+++ Assets/Scripts/Offscreen Indicator/OffScreenIndicator.cs
@@ -62,7 +62,7 @@ public class OffScreenIndicator : MonoBehaviour
             if(indicator)
             {
                 indicator.SetImageColor(target.TargetColor);// Sets the image color of the indicator.
-                indicator.SetDistanceText(distanceFromCamera); //Set the distance text for the indicator.
+                //indicator.SetDistanceText(distanceFromCamera); //Set the distance text for the indicator.
                 indicator.transform.position = screenPosition; //Sets the position of the indicator on the screen.
                 indicator.SetTextRotation(Quaternion.identity); // Sets the rotation of the distance text of the indicator.
             }
