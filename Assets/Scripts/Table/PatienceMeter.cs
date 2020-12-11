ba4e37098864d9d352804c0d45edff26c7a8cdab
shifatkhan <shifatkhan6@gmail.com>
2020-12-11 00:13:35 -0500

    Implemented indicators changing based on table patience

--- Assets/Scripts/Table/PatienceMeter.cs
+++ Assets/Scripts/Table/PatienceMeter.cs
@@ -11,9 +11,17 @@ public class PatienceMeter : MonoBehaviour
     private float patience = 1f;
     private bool isActive = false;
 
+    private Target targetIndicator;
+
     void Awake()
     {
         tableStateAnim = transform.Find("Table State UI/Bubble/Customer Patience").GetComponent<Animator>();
+        targetIndicator = GetComponentInChildren<Target>();
+    }
+
+    private void Start()
+    {
+        targetIndicator.enabled = false;
     }
 
     void Update()
@@ -26,24 +34,34 @@ public class PatienceMeter : MonoBehaviour
             // check patience state
             if (patience > 0.5 && patience <= 1)
             {
+                targetIndicator.enabled = false;
                 tableStateAnim.SetBool("HighPatience", true);
                 tableStateAnim.SetBool("MediumPatience", false);
                 tableStateAnim.SetBool("LowPatience", false);
             }
             else if (patience > 0.2 && patience <= 0.5)
             {
+                // Orange indicator
+                targetIndicator.enabled = true;
+                targetIndicator.ChangeColor(new Color(255f, 165, 0));
+
                 tableStateAnim.SetBool("HighPatience", false);
                 tableStateAnim.SetBool("MediumPatience", true);
                 tableStateAnim.SetBool("LowPatience", false);
             }
             else if (patience > 0 && patience <= 0.2)
             {
+                // Red indicator
+                targetIndicator.enabled = true;
+                targetIndicator.ChangeColor(Color.red);
+
                 tableStateAnim.SetBool("HighPatience", false);
                 tableStateAnim.SetBool("MediumPatience", false);
                 tableStateAnim.SetBool("LowPatience", true);
             }
             else if (patience <= 0)
             {
+                targetIndicator.enabled = false;
                 tableStateAnim.SetBool("HighPatience", false);
                 tableStateAnim.SetBool("MediumPatience", false);
                 tableStateAnim.SetBool("LowPatience", false);
